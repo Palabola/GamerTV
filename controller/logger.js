9ef8becbc91526fd71e5530c79e131067699a918
Palabola <Palabola@users.noreply.github.com>
2016-10-10 17:28:13 +0200

    Logger Cache ver 0.5!
    Todo:
    - Allow for important channels!

--- controller/logger.js
+++ controller/logger.js
@@ -2,16 +2,44 @@
 var db = require('./db_connect');
 
 
+var cache_data = [];
+
+
 function insert_channel(data){
  
- for(var i=0; i < data.length; i++)
-        {  
-          var post  = {name:data[i].channel.name, data: JSON.stringify(data[i])};
-          db.query('INSERT INTO channel_meta SET ?', post, function(err, result) {
-             if (err) throw err;
-           }); 
-       }  
+        for(var i=0; i < data.length; i++)
+               {
+                 var skip = 0;  
+
+               if(typeof(cache_data)!=='undefined') 
+               {
+                        for(var k=0; k < cache_data.length; k++)   
+                        {
+                          if(cache_data[i]._id === data[i]._id)
+                              {
+                                 console.log('skip');
+                                 skip = 1;
+                                 break;
+                              }
+                        }
+                }
+                
+                 if(skip === 0)
+                  {
+                       var post  = {name:data[i].channel.name, data: JSON.stringify(data[i])};
+                       db.query('INSERT INTO channel_meta SET ?', post, function(err, result) {
+                          if (err) throw err;
+                        });
+                   } 
+
+                }
+                
+        cache_data = data;
+        
+        return;
+    
 };
+
 
 
 module.exports.insert_channel = insert_channel;
